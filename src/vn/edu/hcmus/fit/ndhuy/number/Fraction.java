641c61d760e041bd9962dc515b8186899be8792f
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2021-06-13 11:37:13 +0700

    [Basic] Fraction class

---
+++ src/vn/edu/hcmus/fit/ndhuy/number/Fraction.java
@@ -0,0 +1,127 @@
+package vn.edu.hcmus.fit.ndhuy.number;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+
+/**
+ * vn.edu.hcmus.fit.ndhuy.fraction
+ *
+ * Created by ndhuy
+ * Date 13/06/2021 - 10:48
+ * Description: ...
+ */
+
+public class Fraction {
+    private int numerator;
+    private int denominator;
+
+    public Fraction() {
+        this.numerator = 0;
+        this.denominator = 1;
+    }
+
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public Fraction(Fraction fraction) {
+        if (fraction.denominator != 0) {
+            this.numerator = fraction.numerator;
+            this.denominator = fraction.denominator;
+        } else {
+            this.numerator = 0;
+            this.denominator = 1;
+        }
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public void setDenominator(int denominator) {
+        if (denominator != 0)
+            this.denominator = denominator;
+    }
+
+    public Fraction Sum(Fraction fraction) {
+        Fraction result = new Fraction();
+        result.numerator = (this.numerator * fraction.denominator) + (this.denominator * fraction.numerator);
+        result.numerator = this.denominator * fraction.denominator;
+        return Optimize(result);
+    }
+
+    public Fraction Sub(Fraction fraction) {
+        Fraction result = new Fraction();
+        result.numerator = (this.numerator * fraction.denominator) - (this.denominator * fraction.numerator);
+        result.numerator = this.denominator * fraction.denominator;
+        return Optimize(result);
+    }
+
+    public Fraction Mul(Fraction fraction) {
+        Fraction result = new Fraction();
+        result.numerator = this.numerator * fraction.numerator;
+        result.numerator = this.denominator * fraction.denominator;
+        return Optimize(result);
+    }
+
+    public Fraction Div(Fraction fraction) {
+        if (fraction.numerator != 0) {
+            Fraction result = new Fraction();
+            result.numerator = this.numerator * fraction.denominator;
+            result.numerator = this.denominator * fraction.numerator;
+            return Optimize(result);
+        } else {
+            return null;
+        }
+    }
+
+    public int FindLargestCommonDivision(int a, int b) {
+        while(a != 0 && b != 0) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+
+        return a + b;
+    }
+
+    public Fraction Optimize(Fraction fraction) {
+        int largestCommonDivision = FindLargestCommonDivision(fraction.numerator, fraction.denominator);
+        fraction.numerator = fraction.numerator / largestCommonDivision;
+        fraction.denominator = fraction.denominator / largestCommonDivision;
+        return fraction;
+    }
+
+    public boolean Equal(Fraction fraction) {
+        return (this.numerator * fraction.denominator) == (this.denominator * fraction.numerator);
+    }
+
+    public boolean Less(Fraction fraction) {
+        return (this.numerator * fraction.denominator) < (this.denominator * fraction.numerator);
+    }
+
+    public boolean Greater(Fraction fraction) {
+        return (this.numerator * fraction.denominator) > (this.denominator * fraction.numerator);
+    }
+
+    public boolean LessOrEqual(Fraction fraction) {
+        return (this.numerator * fraction.denominator) <= (this.denominator * fraction.numerator);
+    }
+
+    public boolean GreaterOrEqual(Fraction fraction) {
+        return (this.numerator * fraction.denominator) >= (this.denominator * fraction.numerator);
+    }
+
+    public String toString() {
+        return "(" + Integer.toString(this.numerator) + "/" + Integer.toString(this.denominator) + ")";
+    }
+}
--- src/vn/edu/hcmus/fit/ndhuy/se_06_java_oop/Main.java
+++ src/vn/edu/hcmus/fit/ndhuy/se_06_java_oop/Main.java
@@ -1,8 +1,21 @@
 package vn.edu.hcmus.fit.ndhuy.se_06_java_oop;
 
+import vn.edu.hcmus.fit.ndhuy.number.Fraction;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Fraction frac_1 = new Fraction();
+        Fraction frac_2 = new Fraction(4, 8);
+        System.out.println(frac_1);
+        System.out.println(frac_2);
+        Fraction frac_3 = frac_1.Sum(frac_2);
+        System.out.println(frac_3);
+        if (frac_1.Equal(frac_2))
+            System.out.println(frac_1 + " is equal " + frac_2);
+        else if(frac_1.Less(frac_2))
+            System.out.println(frac_1 + " is less than " + frac_2);
+        else
+            System.out.println(frac_1 + " is greater than " + frac_2);
     }
 }
