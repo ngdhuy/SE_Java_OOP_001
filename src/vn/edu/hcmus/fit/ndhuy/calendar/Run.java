0d30b4f6f4f419ad97435681f39718c994c26f0b
Huy Nguyen <ndhuy@fit.hcmus.edu.vn>
2021-07-23 13:39:43 +0700

    [Basic] MyCalendar

---
+++ src/vn/edu/hcmus/fit/ndhuy/calendar/Run.java
@@ -0,0 +1,15 @@
+package vn.edu.hcmus.fit.ndhuy.calendar;
+
+/**
+ * vn.edu.hcmus.fit.ndhuy.calendar
+ * Created by ndhuy
+ * Date 23/07/2021 - 12:40
+ * Description: ...
+ */
+public class Run {
+    public static void run() {
+        MyCalendar cal = new MyCalendar();
+        cal.input();
+        cal.printCalendar();
+    }
+}
--- src/vn/edu/hcmus/fit/ndhuy/se_06_java_oop/Main.java
+++ src/vn/edu/hcmus/fit/ndhuy/se_06_java_oop/Main.java
@@ -1,31 +1,9 @@
 package vn.edu.hcmus.fit.ndhuy.se_06_java_oop;
 
-import com.sun.source.tree.TryTree;
-import org.w3c.dom.css.Rect;
-import vn.edu.hcmus.fit.ndhuy.polygon.*;
-import vn.edu.hcmus.fit.ndhuy.polygon.Rectangle;
-
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.StringJoiner;
-import java.util.concurrent.RecursiveAction;
+import vn.edu.hcmus.fit.ndhuy.calendar.Run;
 
 public class Main {
-
     public static void main(String[] args) {
-        ArrayList<PolygonInterface> arrPolygon = new ArrayList<PolygonInterface>();
-        Circle c = new Circle(new Point2D(2, 3), 5);
-        Rectangle r1 = new Rectangle(new Point2D(4, 7), 18, 20);
-        Rectangle r2 = new Rectangle(new Point2D(1, 1), 5, 8);
-        Triangle t = new Triangle(new Point2D(0, 0), new Point2D(2, 0), new Point2D(0, 3));
-
-        arrPolygon.add(c);
-        arrPolygon.add(r1);
-        arrPolygon.add(r2);
-        arrPolygon.add(t);
-
-        for (PolygonInterface poly : arrPolygon) {
-            System.out.println(poly);
-        }
+        Run.run();
     }
 }
